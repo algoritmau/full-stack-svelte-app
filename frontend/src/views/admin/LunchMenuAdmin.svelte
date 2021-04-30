b15440e7995dd356e08eb87267a6984ea442b1ef
Mauricio Paternina <spaceinvadev@gmail.com>
2021-04-30 12:49:23 -0500

    set up app routing

---
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -0,0 +1,7 @@
+<script>
+  let schoolName = "Test School"
+</script>
+
+<main>
+  <h1 class="title">Lunch Menu Admin Page / {schoolName}</h1>
+</main>
---
+++ frontend/src/views/public/LunchMenuView.svelte
@@ -0,0 +1,3 @@
+<main>
+  <h1 class="title">Lunch Menu Page</h1>
+</main>
